0fd328583a01eadaf8b244f165fad50a1098bb8b
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2025-01-28 19:35:52 +0200

    initial commit

---
+++ src/test/java/com/kodarit/stars/toolbox/ToolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodarit.stars.toolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
