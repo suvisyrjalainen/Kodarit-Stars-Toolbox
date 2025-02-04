b9a6b57872b8035d9170ff6b46f0b2977a506fac
MattiDragon <matias.koivikko@gmail.com>
2025-02-04 19:55:25 +0200

    Set up layout template and use that for 2 pages

--- src/main/java/com/kodarit/stars/toolbox/ToolboxController.java
+++ src/main/java/com/kodarit/stars/toolbox/ToolboxController.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ToolboxController {
+    // Set up a basic mapping that maps a path (/) to a template file.
+    // The method name is irrelevant but should be reasonable
+    // We can do more advanced logic in these methods later,
+    // but for now we just return the name of a template.
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 }
