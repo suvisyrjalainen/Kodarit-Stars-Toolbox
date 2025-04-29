c8956590430a34639a0b1d7bc1607a9e39b1e000
MattiDragon <matias.koivikko@gmail.com>
2025-04-29 19:06:39 +0300

    Add node types and style some on the json tree page

--- src/main/resources/static/js/json_tree.js
+++ src/main/resources/static/js/json_tree.js
@@ -2,6 +2,8 @@
  * @typedef {"object"|"array"|"string"|"number"|"boolean"|"null"} JsonType
  */
 
+const jsonArea = document.getElementById("json-area");
+
 function loadProvidedJson() {
     const params = new URLSearchParams(document.location.search);
     const providedJson = params.json
@@ -19,12 +21,11 @@ function createValue(type) {
      * @type {HTMLTemplateElement}
      */
     const template = document.getElementById(`json-${type}-template`)
-    switch (type) {
-        case "string":
-            return template.content.cloneNode(true)
-    }
+    const node = template.content.cloneNode(true)
+    jsonArea.appendChild(node)
 }
 
-const s = createValue("string")
-document.getElementById("json-area")
-    .appendChild(s)
+for (const button of document.querySelectorAll(".add-element-button")) {
+    const type = button.dataset.type
+    button.addEventListener("click", () => createValue(type))
+}
